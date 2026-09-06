e27129286c8e59d326750eab3aa88b0af9eeeb54
Nirjara Verma <nirjarav5710@gmail.com>
2026-09-06 20:56:02 +0530

    commit

---
+++ backend-FSD/day2.js
@@ -0,0 +1,7 @@
+// //synchronous:blocking js and asynchronous :non-blocking js
+// // function hello(){
+// //     console.log("task 3");
+// // }
+// hello(){
+//      console.log("task 2");
+//      setTimeout(function(){
