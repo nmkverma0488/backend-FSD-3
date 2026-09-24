915606af1c0dd4b16bb0f349f7fc0856d55665ff
Nirjara Verma <nirjarav5710@gmail.com>
2026-09-25 00:31:15 +0530

    commit

--- ASSIGNMNET/Assign4/Express_react/index.js
+++ ASSIGNMNET/Assign4/Express_react/index.js
@@ -1,44 +1,58 @@
-import express from 'express';
-import cors from 'cors';
-import fs from 'fs';
-import product from product;
-const app=express();
- 
+import express from "express";
+import fs from "fs";
+import cors from "cors";
+
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-//GET
-app.get('/products',(req,res)=>{
-     
-const data=fs.readFile("product.json","utf-8");
-const products=JSON.parse(data);
-res.json(products);
+// GET products
+app.get("/api/products", (req, res) => {
+  const data = fs.readFileSync("products.json", "utf-8");
 
+  const products = JSON.parse(data);
+
+  res.json(products);
 });
 
+// POST product
+app.post("/api/products", (req, res) => {
+  const data = fs.readFileSync("products.json", "utf-8");
 
-//post
+  const products = JSON.parse(data);
 
-app.post('/products',(req,res)=>{
-     
-const data=fs.readFile("product.json","utf-8");
-const products=JSON.parse(data);
-const newProduct={
-    id:products.length+1,
-    name:req.body.name,
-    price:req.body.price
-}
-products.push(new produt);
-fs.writeFile("product.json",JSON.stringify(products),()=>{
-    
-})
-res.json(products);
+  const newProduct = {
+    id: products.length + 1,
+    name: req.body.name,
+    price: req.body.price,
+    category: req.body.category,
+  };
 
+  products.push(newProduct);
+
+  fs.writeFileSync("products.json", JSON.stringify(products, null, 2));
+
+  res.json(newProduct);
 });
-  
-app.post('/products')
-app.listen(4000,()=>{
-      
 
-})
+// DELETE product
+app.delete("/api/products/:id", (req, res) => {
+  const data = fs.readFileSync("products.json", "utf-8");
+
+  let products = JSON.parse(data);
+
+  const id = parseInt(req.params.id);
+
+  products = products.filter((product) => product.id !== id);
+
+  fs.writeFileSync("products.json", JSON.stringify(products, null, 2));
+
+  res.json({
+    message: "Product deleted successfully",
+  });
+});
+
+app.listen(5000, () => {
+  console.log("Server running on http://localhost:5000");
+});
